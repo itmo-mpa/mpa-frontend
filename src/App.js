b631a0a867c008b1c24a2caf2e7b7c40be3ffd93
gohabereg <gohabereg@gmail.com>
2019-05-26 17:05:07 +0300

    Add f*cking state management

--- src/App.js
+++ src/App.js
@@ -8,28 +8,21 @@ import SidebarList from './components/SidebarList/SidebarList';
 export default class App extends React.Component {
   state = {
       burgerRef: null,
-      patient: {}
   };
-    patientOnChange = (patient) => {
-        this.setState({ patient });
-    };
 
     render () {
-        const { patient } = this.state;
-        console.log('app patient', patient);
-
         return (
             <Sidebar.Pushable as={Segment}>
-                <SidebarList trigger={this.state.burgerRef} patientOnChange={this.patientOnChange}/>
+                <SidebarList trigger={this.state.burgerRef} />
                 <Sidebar.Pusher className="App">
                     <Menu attached="top">
                         <Ref innerRef={burger => this.setState({ burgerRef: burger })}>
                             <Menu.Item icon="bars" />
                         </Ref>
                     </Menu>
-                    {patient && <PatientInfo patient={patient}/>}
+                    <PatientInfo />
                     {/* принимаем на веру то, что бек создаёт статус при создании пациента */}
-                    {patient && <States patientId={patient.id} status={patient.status}/> }
+                    <States />
                 </Sidebar.Pusher>
             </Sidebar.Pushable>
         );
