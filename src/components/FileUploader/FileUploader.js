bfb5a73ae4a4f1bf8bb333759e822dbd3f54df85
olyalex <oljaaleks@yandex.ru>
2019-12-18 20:21:26 +0300

    Added file uploader

--- src/components/FileUploader/FileUploader.js
+++ src/components/FileUploader/FileUploader.js
@@ -4,55 +4,55 @@ import './FileUploader.css';
 import StatusDraftContainer from '../StatusDraft/StatusDraft';
 
 export class FileUploader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isModalOpen: false,
-                  file: '',
-                  fileName: ''
-                };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.fileInput = React.createRef();
-  }
+    constructor (props) {
+        super(props);
+        this.state = { isModalOpen: false,
+            file: '',
+            fileName: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.fileInput = React.createRef();
+    }
   closeModal = () => {
       this.setState({ isModalOpen: false });
   }
 
-  handleSubmit(event) {
-    this.setState({file: this.fileInput.current.files[0],
-                  isModalOpen: false,
-                  fileName: this.fileInput.current.files[0].name
-                });
-    event.preventDefault();
+  handleSubmit (event) {
+      this.setState({ file: this.fileInput.current.files[0],
+          isModalOpen: false,
+          fileName: this.fileInput.current.files[0].name
+      });
+      event.preventDefault();
   }
-      
-  render() {
-    return (
-      <div>
-        <p>{this.state.fileName}</p>
-        <Modal
-        trigger={
-          <button onClick={() => { this.setState({ isModalOpen: true }); }}>Загрузить данные о проведенных процедурах</button>
-              }
-          open={this.state.isModalOpen}
-          onClose={this.closeModal}
-        >
-          <h3>Данные о проведенных процедурах</h3>
-          <select>
-            <option>ПЭТ</option>
-            <option>МРТ</option>
-          </select>
-          <form onSubmit={this.handleSubmit}>
-            <input className="fileInput" 
-            type="file"
-            ref={this.fileInput}
-            />
-          <button className="submitButton" 
-            type="submit"
-            >Загрузить файл</button>
-          </form>
-        </Modal>
-      </div>
-    );
+
+  render () {
+      return (
+          <div>
+              <p>{this.state.fileName}</p>
+              <Modal
+                  trigger={
+                      <button onClick={() => { this.setState({ isModalOpen: true }); }}>Загрузить данные о проведенных процедурах</button>
+                  }
+                  open={this.state.isModalOpen}
+                  onClose={this.closeModal}
+              >
+                  <h3>Данные о проведенных процедурах</h3>
+                  <select>
+                      <option>ПЭТ</option>
+                      <option>МРТ</option>
+                  </select>
+                  <form onSubmit={this.handleSubmit}>
+                      <input className="fileInput"
+                          type="file"
+                          ref={this.fileInput}
+                      />
+                      <button className="submitButton"
+                          type="submit"
+                      >Загрузить файл</button>
+                  </form>
+              </Modal>
+          </div>
+      );
   }
 }
 
